af9c5ab146d4f680b34f99f52dc6ac0be558290e
Jia Nan <KoiGoko@outlook.com>
2023-05-04 15:05:39 +0800

    2023 5 4

--- src/components/main/card/IslandMain.vue
+++ src/components/main/card/IslandMain.vue
@@ -1,11 +1,51 @@
 <template>
-
+  <v-card width="300" class="search-card rounded">
+    <v-text-field
+      bg-color="white"
+      v-model="searchText"
+      :placeholder="placeholderText"
+      rounded
+      dense
+      class="search-field no-focus-outline rounded-0"
+    />
+    <v-btn variant="flat" icon color="white" class="search-btn rounded-0">
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn variant="flat" icon color="white" class="search-btn rounded-0">
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-card>
 </template>
 
 <script>
-
+export default {
+  name: "SearchBox",
+  data() {
+    return {
+      searchText: "",
+      placeholderText: "Search",
+    };
+  },
+};
 </script>
 
 <style scoped>
+.search-card {
+  background-color: white;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 10px;
+  left: 10px;
+}
 
+.search-field {
+  flex-grow: 1;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+.search-btn {
+  height: 100%;
+}
 </style>
--- src/components/main/card/index/IndexIsland.vue
+++ src/components/main/card/index/IndexIsland.vue
@@ -1,11 +1,20 @@
 <template>
-
+  <v-menu
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props">Show Menu</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        My Menu
+      </v-card-title>
+      <v-card-text>
+        This is my menu.
+      </v-card-text>
+    </v-card>
+  </v-menu>
 </template>
-
-<script>
-
+<script setup lang="ts">
 </script>
-
-<style scoped>
-
+<style>
 </style>
