a21bb1565ff88e79bdb53be5eb284c8075f0a1ce
Jia Nan <KoiGoko@outlook.com>
2023-05-05 15:16:28 +0800

    2023 5 5

---
+++ src/components/main/card/index/SearchPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SearchResultPage from "@/components/main/card/index/SearchResultPage.vue";
+import SearchBox from "@/components/main/card/index/SearchBox.vue";
+import {ref} from "vue";
+const showSearchResult = ref(false)
+</script>
+<template>
+  <div>
+    <SearchBox>
+      <v-btn @click="showSearchResult = true" variant="flat" icon color="white" class="search-btn rounded-0">
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </SearchBox>
+    <SearchResultPage v-if="showSearchResult"/>
+  </div>
+</template>
+<style scoped>
+
+</style>
---
+++ src/components/main/card/index/SearchResultPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <v-card class="search-page" color="white" width="350" height="100%">
+    </v-card>
+  </div>
+</template>
+
+<style scoped>
+.search-page {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+</style>
+
