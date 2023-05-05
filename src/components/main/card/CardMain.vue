37b96dec0a7059dd003a448d582d3009d58a2099
Jia Nan <KoiGoko@outlook.com>
2023-05-05 09:48:23 +0800

    2023 5 5

--- src/components/main/card/CardMain.vue
+++ src/components/main/card/CardMain.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-card color="primary" class="d-flex rounded-0" width="300" height="100%">
-    <v-card height="50" width="100%" color="primary"></v-card>
+  <v-card color="primary" class="d-flex rounded-0 align-center" width="300" height="100%">
+    <v-btn
+      color="red"
+      class="d-flex circle-btn"
+      icon="mdi-plus"
+    >
+    </v-btn>
   </v-card>
 </template>
 
@@ -8,7 +13,11 @@
 </script>
 
 <style scoped>
-.contain {
-  height: 100%;
+.circle-btn {
+  position: absolute;
+  right: -20px;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 1;
 }
 </style>
