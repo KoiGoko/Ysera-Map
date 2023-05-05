f6e60821e4468265ea20a8274a27e89e0d00dd20
Jia Nan <KoiGoko@outlook.com>
2023-05-05 13:21:01 +0800

    2023 5 5

--- src/components/main/drawer/left_drawer/LeftNuclear.vue
+++ src/components/main/drawer/left_drawer/LeftNuclear.vue
@@ -1,17 +1,21 @@
 <template>
   <DrawMain>
     <v-list density="compact" nav>
-      <v-list-item @click="singlePush" class="rounded-end-pill" :prepend-icon="icons.dose_single" title="个人剂量" value="dose_single"></v-list-item>
-      <v-list-item @click="mutilatePush" class="rounded-end-pill" :prepend-icon="icons.dose_mutilate" title="集体计量" value="dose_mutilate"></v-list-item>
-      <v-list-item @click="otherPush" class="rounded-end-pill" :prepend-icon="icons.other" title="其他" value="other"></v-list-item>
+      <v-list-item @click="singlePush" class="rounded-end-pill" :prepend-icon="icons.dose_single" title="个人剂量"
+                   value="dose_single"></v-list-item>
+      <v-list-item @click="mutilatePush" class="rounded-end-pill" :prepend-icon="icons.dose_mutilate" title="集体计量"
+                   value="dose_mutilate"></v-list-item>
+      <v-list-item @click="otherPush" class="rounded-end-pill" :prepend-icon="icons.other" title="其他"
+                   value="other"></v-list-item>
     </v-list>
   </DrawMain>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import DrawMain from "@/components/main/drawer/DrawMain.vue";
 import {useDrawerIcons} from "@/utils/useDrawerIcons"
+
 const router = useRouter()
 const {icons, toggleIcon} = useDrawerIcons({
   icons: {
@@ -30,21 +34,24 @@ const {icons, toggleIcon} = useDrawerIcons({
     other: 'mdi-circle-outline'
   }
 })
+
 function singlePush() {
   router.push({
-    name:'single'
+    name: 'single'
   })
   toggleIcon('dose_single')
 }
+
 function mutilatePush() {
   router.push({
-    name:'multiple'
+    name: 'multiple'
   })
   toggleIcon('dose_mutilate')
 }
+
 function otherPush() {
   router.push({
-    name:'other'
+    name: 'other'
   })
   toggleIcon('other')
 }
