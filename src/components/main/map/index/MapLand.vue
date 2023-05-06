5efd2911ed5603f8987e89951ede786750060f43
Jia Nan <KoiGoko@outlook.com>
2023-05-06 15:44:26 +0800

    2023 5 6

--- src/components/main/map/index/MapLand.vue
+++ src/components/main/map/index/MapLand.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <MapMain></MapMain>
+    <MapMain>
+<!--      <v-raster-source-->
+<!--        id="tiles"-->
+<!--        :tiles="[-->
+<!--          'http://localhost:8089/data/beijingesa/{z}/{x}/{y}.png',-->
+<!--        ]"-->
+<!--      />-->
+<!--      <v-raster-layer id="tiles" source="tiles" />-->
+    </MapMain>
+    <RightFunctionMain/>
   </div>
 </template>
 
 <script setup lang="ts">
 import MapMain from "@/components/main/map/MapMain.vue";
+import RightFunctionMain from "@/components/main/utils/RightFunctionMain.vue";
 </script>
 
 <style scoped>
---
+++ src/components/main/utils/RightFunctionMain.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="function-card">
+    <v-card class="rounded-pill" color="white" width="50" height="300">
+    </v-card>
+  </div>
+
+</template>
+
+<style scoped>
+.function-card {
+    position: absolute;
+    top: 80px;
+    right: 20px;
+}
+</style>
