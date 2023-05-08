f22d1caf357f49acde1c1661c1179fa8c333cc28
Jia Nan <KoiGoko@outlook.com>
2023-05-08 10:59:00 +0800

    2023 5 8

--- src/components/main/map/index/MapLand.vue
+++ src/components/main/map/index/MapLand.vue
@@ -1,22 +1,81 @@
 <template>
-  <div>
-    <MapMain>
-<!--      <v-raster-source-->
-<!--        id="tiles"-->
-<!--        :tiles="[-->
-<!--          'http://localhost:8089/data/beijingesa/{z}/{x}/{y}.png',-->
-<!--        ]"-->
-<!--      />-->
-<!--      <v-raster-layer id="tiles" source="tiles" />-->
-    </MapMain>
-    <RightFunctionMain/>
-  </div>
+  <v-map
+    ref="mapRef"
+    :accessToken="accessToken"
+    :options="options"
+  >
+    <MapScale/>
+    <SwitchBgMap @receiveChild="receiveChild"/>
+    <v-vector-source
+      id="mapbox-terrain"
+      url="mapbox://mapbox.mapbox-terrain-v2"
+    />
+    <v-fill-layer
+      id="landcover"
+      source="mapbox-terrain"
+      sourceLayer="landcover"
+      :paint="{
+        'fill-color': 'rgba(66,100,251, 0.3)',
+        'fill-outline-color': 'rgba(66,100,251, 1)'
+      }"
+    />
+    <v-fill-layer
+      id="hillshade"
+      source="mapbox-terrain"
+      sourceLayer="hillshade"
+      :paint="{
+        'fill-color': 'rgba(66,100,251, 0.3)',
+        'fill-outline-color': 'rgba(66,100,251, 1)'
+      }"
+    />
+    <v-line-layer
+      id="contour"
+      source="mapbox-terrain"
+      sourceLayer="contour"
+      :paint="{
+        'line-color': '#ffffff',
+      }"
+    />
+    <div class="scale-button d-flex flex-column justify-center align-center">
+      <v-btn @click="zoomIn" icon="mdi-plus"></v-btn>
+      <v-btn @click="zoomOut" icon="mdi-reduce"></v-btn>
+    </div>
+  </v-map>
 </template>
 
 <script setup lang="ts">
-import MapMain from "@/components/main/map/MapMain.vue";
-import RightFunctionMain from "@/components/main/utils/RightFunctionMain.vue";
+import {ref, reactive} from "vue";
+import {accessToken} from "@/utils/mapUtils"
+import MapScale from "@/components/main/utils/MapScale.vue";
+import SwitchBgMap from "@/components/main/utils/SwitchBgMap.vue";
+
+const options = reactive({
+  center: [120, 30],
+  zoom: 5,
+  style: 'mapbox://styles/mapbox/light-v11',
+  projection: 'mercator'
+})
+const mapRef = ref();
+
+function zoomIn() {
+  options.zoom++
+}
+
+function zoomOut() {
+  options.zoom--
+}
+
+function receiveChild(value: string) {
+  options.style = value;
+}
+
 </script>
 
 <style scoped>
+.scale-button {
+  position: absolute;
+  bottom: 160px;
+  right: 20px;
+}
+
 </style>
--- src/components/main/utils/MapScale.vue
+++ src/components/main/utils/MapScale.vue
@@ -1,14 +1,10 @@
 <template>
   <!--map scale-->
-  <v-scale-control class="scale-control" position="bottom-right"/>
+  <v-card color="blue"><v-scale-control class="scale-control" position="bottom-right"/></v-card>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
-.scale-control {
-  position: absolute;
-  right: 20px;
-}
 </style>
